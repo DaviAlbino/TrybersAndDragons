8c3c4841f4f93b5b94d4529ceaef53986be38a38
Davi Albino <negroalbino_@hotmail.com>
2023-01-20 11:21:52 -0300

    Now they're ready to Fight

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -3,14 +3,16 @@ import Battle from './Battle';
 
 export default class PVP extends Battle {
   private _player2: Fighter;
+  private _player1: Fighter;
 
   constructor(player1: Fighter, player2: Fighter) {
     super(player1);
+    this._player1 = player1;
     this._player2 = player2;
   }
 
   get player1(): Fighter {
-    return this.player1;
+    return this._player1;
   }
 
   get player2(): Fighter {
@@ -22,9 +24,9 @@ export default class PVP extends Battle {
   } 
 
   fight(): number {
-    if (this.theyAreAlive()) {
+    while (this.theyAreAlive()) {
       this.player1.attack(this.player2);
-      if (this.theyAreAlive()) this._player2.attack(this.player1);
+      this.player2.attack(this.player1);
     }
 
     return super.fight();
